5112f4654a2be421bb6622afa492bd6d34ddcfdb
A. Matías Quezada <amatiasq@gmail.com>
2017-12-13 21:27:03 +0100

    Missing files

---
+++ src/triggers/10-sangrad.js
@@ -0,0 +1,20 @@
+'use strict';
+const { contains, normalize, random } = require('../util');
+const genara = require('../genara');
+
+
+const mafrunes = [
+    'http://blog.panreyes.com/sangrad.png',
+    'http://repos.amatiasq.com/genara/sangrad.jpeg',
+];
+
+
+genara.trigger(async(message) => {
+    if (contains(normalize(message.content), 'sangrad')) {
+        return message.channel.send({
+            embed: {
+                image: { url: random(mafrunes) }
+            }
+        });
+    }
+});
