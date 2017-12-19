3205d0a1a92dc43ce1d865169691c65f21191933
A. Matías Quezada <amatiasq@gmail.com>
2017-12-19 10:38:38 +0100

    Fixes and meh

--- src/antuan/commands/90-cuantos-piropos-sabes.js
+++ src/antuan/commands/90-cuantos-piropos-sabes.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = async(antuan, message, text, { randomItem }) => {
-    return message.reply(`${antuan.memory.get('piropo', []).length} ${randomItem([
+    return message.reply(`${antuan.memory.get('piropos', []).length} ${randomItem([
         'pero a ti te seduzco con la mirada',
     ])}`);
 };
