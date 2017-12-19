45654e7140931957e51af34baeaeb56bda2cb91f
A. Matías Quezada <amatiasq@gmail.com>
2017-12-19 12:09:08 +0100

    Stuff

--- src/maram/index.js
+++ src/maram/index.js
@@ -21,4 +21,6 @@ const maram = new Bot([
 maram.command('hola', async(bot, message) => message.channel.send('Hola, soy Maram'));
 maram.command('dame el link', async(bot, message) => message.channel.send('https://discordapp.com/oauth2/authorize?client_id=391244013421854722&scope=bot&permissions=281664'));
 
+maram.alias('por cuanto tiempo', 'hasta cuando');
+
 module.exports = require('../common')(maram);
