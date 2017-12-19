0cda1d8231f55bf4344a32cfd48ba3fddae2b29f
A. Matías Quezada <amatiasq@gmail.com>
2017-12-19 21:20:21 +0100

    Stuff

--- src/antuan/commands/10-pelea.js
+++ src/antuan/commands/10-pelea.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = async(antuan, message, text, { containsWord, randomItem }) => {
-    return antuan.fight(message, 'secret', containsWord(text, 'pro') ? 'master' : 'normal');
+    return antuan.fight(
+        message,
+        containsWord(text, 'idiota') ? 'third' : 'secret',
+        containsWord(text, 'pro') ? 'master' : 'normal'
+    );
 };
