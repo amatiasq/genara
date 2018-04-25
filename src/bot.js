c96ca342e430569833d6ecdc5652d3fb5b996e8b
A. Matías Quezada <amatiasq@gmail.com>
2018-04-25 21:21:52 +0200

    Fix unresolved require

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const padLeft = require('left-pad');
 const Discord = require('discord.js');
-const Memory = require('./memory');
 const util = require('./util');
 const logged = new Map();
 
